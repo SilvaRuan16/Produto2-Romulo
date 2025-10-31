0aff2d390e88d1e0760ff4dd44dfa2935767c502
SilvaRuan16 <154707674+SilvaRuan16@users.noreply.github.com>
2025-10-30 21:10:48 -0300

    feat: Criação da estrutura do projeto, pastas de controllers, models, repository e services. Também foi feito a Classe Usuario para model

---
+++ src/test/java/com/boletin/aplication/ocorrencia/OcorrenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boletin.aplication.ocorrencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcorrenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
